18449d4b1d108c0e605cf1d364ab2a9e05e04df1
ageuxo <ageuxo@gmail.com>
2023-02-16 15:02:14 +0100

    Initial commit

---
+++ src/main/java/MagicWords/MagicWords.java
@@ -0,0 +1,36 @@
+package MagicWords;
+
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(MagicWords.MODID)
+public class MagicWords {
+    public static final String MODID = "magicwords";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public MagicWords(){
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event){
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event){
+
+        }
+    }
+
+}
