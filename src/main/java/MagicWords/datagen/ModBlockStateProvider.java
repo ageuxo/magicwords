d20b13b482fe07961518f6881d89598030fdf05a
ageuxo <ageuxo@gmail.com>
2023-02-24 19:21:15 +0100

    Added data generation (poorly) (doesn't work??) (no error)

---
+++ src/main/java/MagicWords/datagen/ModBlockStateProvider.java
@@ -0,0 +1,28 @@
+package MagicWords.datagen;
+
+import MagicWords.MagicWords;
+import MagicWords.block.ModBlocks;
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, MagicWords.MODID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.CHALK_BLOCK);
+        blockWithItem(ModBlocks.DUMMY_BLOCK);
+        blockWithItem(ModBlocks.DUMMY_CLAY_BLOCK);
+
+
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject){
+        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    }
+}
