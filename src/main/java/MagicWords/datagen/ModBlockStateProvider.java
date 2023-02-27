5e997d54b4b61ec1cc0da192d9785bf7e25de6fe
ageuxo <ageuxo@gmail.com>
2023-02-27 22:22:33 +0100

    Cleanup

--- src/main/java/MagicWords/datagen/ModBlockStateProvider.java
+++ src/main/java/MagicWords/datagen/ModBlockStateProvider.java
@@ -4,6 +4,7 @@ import MagicWords.MagicWords;
 import MagicWords.block.ModBlocks;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.RegistryObject;
@@ -22,5 +23,8 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
     private void blockWithItem(RegistryObject<Block> blockRegistryObject){
         simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    }
+    private void logBlockWithItem(RegistryObject<RotatedPillarBlock> blockRegistryObject){
+        logBlock(blockRegistryObject.get());
     }
 }
